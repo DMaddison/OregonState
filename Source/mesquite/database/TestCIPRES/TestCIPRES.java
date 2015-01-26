27bf4c973ab05cf0102335ad4ea905ca907ce87e
jcoliver <jeffrey.oliver@science.oregonstate.edu>
2015-01-26 13:28:34 -0800

    Fix CIPRES test so authentication succeeds.

--- Source/mesquite/database/TestCIPRES/TestCIPRES.java
+++ Source/mesquite/database/TestCIPRES/TestCIPRES.java
@@ -1,8 +1,9 @@
 package mesquite.database.TestCIPRES;
 
-import java.io.IOException;
+import java.io.*;
 
 import org.apache.commons.codec.binary.Base64;
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
@@ -45,51 +46,6 @@ public class TestCIPRES extends UtilitiesAssistant {
 		// URL for list of tools (no authentication necessary)
 		String toolURL = baseURL + "/tool";
 
-		HttpClient httpclient = HttpClientBuilder.create().build();
-		
-        try {
-        	Debugg.println("||||||||||||||||||||||||||");
-        	Debugg.println("tool URL: \n" + toolURL);
-        	Debugg.println("list URL: \n" + listURL);
-        	HttpGet httpget = new HttpGet(listURL); // Currently returns 404, maybe because missing cipres-appkey header
-        	//HttpGet httpget = new HttpGet(toolURL); // This should return 200
-            
-            // Setting up the value of the Authentication header
-            final String plainCreds = username + ":" + password;
-            final byte[] plainCredsBytes = plainCreds.getBytes();
-            final byte[] base64CredsBytes = Base64.encodeBase64(plainCredsBytes);
-            final String base64Creds = new String(base64CredsBytes);
-            httpget.addHeader("Authentication", "Basic " + base64Creds);
-            httpget.addHeader("cipres-appkey", CIPRESkey); //Need a value for this header
-            
-            Debugg.println("Executing request " + httpget.getRequestLine());
-            HttpResponse response = httpclient.execute(httpget);
-            try {
-            	Debugg.println("-----------------RESPONSE-----------------------");
-            	Debugg.println(response.getStatusLine().toString());
-                // Would do something with response here...
-            	Debugg.println("----------------------------------------------------");
-
-                EntityUtils.consume(response.getEntity());
-            } catch (IOException e) {
-            	Debugg.println("IOException: "+ e.toString());
-            	e.printStackTrace();
-            } catch (Exception e) {
-            	Debugg.println("Exception: "+ e.toString());
-            	e.printStackTrace();
-            }
-        } catch (ClientProtocolException e) {
-        	Debugg.println("ClientProtocolException: "+ e.toString());
-       	e.printStackTrace();
-        } catch (IOException e) {
-        	Debugg.println("IOException 2: "+ e.toString());
-        	e.printStackTrace();
-        } catch (Exception e) {
-        	Debugg.println("Exception 2: "+ e.toString());
-        	e.printStackTrace();
-        }
-
-		/*
 		// from http://www.artima.com/forums/flat.jsp?forum=121&thread=357685
 		CredentialsProvider provider = new BasicCredentialsProvider();
 		UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(username, password);
@@ -101,14 +57,24 @@ public class TestCIPRES extends UtilitiesAssistant {
         try {
         	HttpResponse response = httpclient.execute(httpget);
         	Debugg.println("-----------------RESPONSE-----------------------");
+        	// Would do something with response here...
         	Debugg.println(response.getStatusLine().toString());
-            // Would do something with response here...
+        	
+        	// Just printing out the response entity.
+        	// To do actually something, would want to use XML / DOM tools
+        	HttpEntity responseEntity = response.getEntity();
+			InputStream instream = responseEntity.getContent();
+			BufferedReader br = new BufferedReader(new InputStreamReader(instream));
+			String line = "";
+			while((line = br.readLine()) != null) {
+				Debugg.println(line);
+			}
+
         	Debugg.println("----------------------------------------------------");
             EntityUtils.consume(response.getEntity());
         } catch (IOException e) {
         	Debugg.printStackTrace(e);
         }
-        */
 	}
 
 	public String getName() {
