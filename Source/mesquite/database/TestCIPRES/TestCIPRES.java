8c4d7aabd70bff5141d98c40a6d5c02a1fe45b0f
David Maddison <david.maddison@science.oregonstate.edu>
2015-01-26 16:06:26 -0800

    Separated listTools and listJobs methods

--- Source/mesquite/database/TestCIPRES/TestCIPRES.java
+++ Source/mesquite/database/TestCIPRES/TestCIPRES.java
@@ -33,36 +33,36 @@ public class TestCIPRES extends UtilitiesAssistant {
 		return null;
 	}
 
-	public void testCIPRes() {
-		String baseURL = "https://www.phylo.org/cipresrest/v1";
-		
-		String username = "DavidMaddison";
-		String password = ""; // we would need to retrieve this from somewhere
-		String CIPRESkey = "Mesquite-7C63884588B8438CAE456E115C9643F3";
-		
-		// URL for a list of jobs
-		String listURL = baseURL + "/job/" + username;
+	String baseURL = "https://www.phylo.org/cipresrest/v1";
+	
+	String username = "DavidMaddison";
+	String password = "rainingPleocoma"; // we would need to retrieve this from somewhere
+	String CIPRESkey = "Mesquite-7C63884588B8438CAE456E115C9643F3";
+	
 
-		// URL for list of tools (no authentication necessary)
-		String toolURL = baseURL + "/tool";
-
+	/*.................................................................................................................*/
+	public HttpClient getHttpClient(){
 		// from http://www.artima.com/forums/flat.jsp?forum=121&thread=357685
 		CredentialsProvider provider = new BasicCredentialsProvider();
 		UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(username, password);
 		provider.setCredentials(AuthScope.ANY, credentials);
-		HttpClient httpclient = HttpClientBuilder.create().setDefaultCredentialsProvider(provider).build();
+		return HttpClientBuilder.create().setDefaultCredentialsProvider(provider).build();
+	}
 
-    	HttpGet httpget = new HttpGet(listURL); // Currently returns 404, maybe because missing cipres-appkey header
-        httpget.addHeader("cipres-appkey", CIPRESkey); //Need a value for this header
-        try {
-        	HttpResponse response = httpclient.execute(httpget);
-        	Debugg.println("-----------------RESPONSE-----------------------");
-        	// Would do something with response here...
-        	Debugg.println(response.getStatusLine().toString());
-        	
-        	// Just printing out the response entity.
-        	// To do actually something, would want to use XML / DOM tools
-        	HttpEntity responseEntity = response.getEntity();
+	/*.................................................................................................................*/
+	public void  listTools(HttpClient httpclient){
+		String URL = baseURL + "/tool";
+		HttpGet httpget = new HttpGet(URL); // Currently returns 404, maybe because missing cipres-appkey header
+		httpget.addHeader("cipres-appkey", CIPRESkey); //Need a value for this header
+		try {
+			HttpResponse response = httpclient.execute(httpget);
+			Debugg.println("-----------------RESPONSE-----------------------");
+			// Would do something with response here...
+			Debugg.println(response.getStatusLine().toString());
+
+			// Just printing out the response entity.
+			// To do actually something, would want to use XML / DOM tools
+			HttpEntity responseEntity = response.getEntity();
 			InputStream instream = responseEntity.getContent();
 			BufferedReader br = new BufferedReader(new InputStreamReader(instream));
 			String line = "";
@@ -70,11 +70,46 @@ public class TestCIPRES extends UtilitiesAssistant {
 				Debugg.println(line);
 			}
 
-        	Debugg.println("----------------------------------------------------");
-            EntityUtils.consume(response.getEntity());
-        } catch (IOException e) {
-        	Debugg.printStackTrace(e);
-        }
+			Debugg.println("----------------------------------------------------");
+			EntityUtils.consume(response.getEntity());
+		} catch (IOException e) {
+			Debugg.printStackTrace(e);
+		}
+	}
+	/*.................................................................................................................*/
+	public void  listJobs(HttpClient httpclient){
+		String URL = baseURL + "/job/" + username;
+		HttpGet httpget = new HttpGet(URL); // Currently returns 404, maybe because missing cipres-appkey header
+		httpget.addHeader("cipres-appkey", CIPRESkey); //Need a value for this header
+		try {
+			HttpResponse response = httpclient.execute(httpget);
+			Debugg.println("-----------------RESPONSE-----------------------");
+			// Would do something with response here...
+			Debugg.println(response.getStatusLine().toString());
+
+			// Just printing out the response entity.
+			// To do actually something, would want to use XML / DOM tools
+			HttpEntity responseEntity = response.getEntity();
+			InputStream instream = responseEntity.getContent();
+			BufferedReader br = new BufferedReader(new InputStreamReader(instream));
+			String line = "";
+			while((line = br.readLine()) != null) {
+				Debugg.println(line);
+			}
+
+			Debugg.println("----------------------------------------------------");
+			EntityUtils.consume(response.getEntity());
+		} catch (IOException e) {
+			Debugg.printStackTrace(e);
+		}
+	}
+
+	/*.................................................................................................................*/
+	public void testCIPRes() {
+		
+		HttpClient httpclient = getHttpClient();
+		listTools(httpclient);
+
 	}
 
 	public String getName() {
